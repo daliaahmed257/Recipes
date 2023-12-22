77e26d735f0fe5a4a869238c055013b7a132d149
Dalia Ahmed <da257@njit.edu>
2023-12-22 13:13:15 -0500

    authentication added

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,34 @@
 var express = require('express');
 var router = express.Router();
+const passport = require('passport');
 
 router.get('/', function(req, res, next) {
   res.redirect('/recipes');
+});
+
+router.get('/auth/google', passport.authenticate(
+  // Which passport strategy is being used?
+  'google',
+  {
+    // Requesting the user's profile and email
+    scope: ['profile', 'email'],
+    // Optionally force pick account every time
+    // prompt: "select_account"
+  }
+));
+
+router.get('/oauth2callback', passport.authenticate(
+  'google',
+  {
+    successRedirect: '/recipes',
+    failureRedirect: '/recipes'
+  }
+));
+
+router.get('/logout', function(req, res){
+  req.logout(function() {
+    res.redirect('/recipes');
+  });
 });
 
 module.exports = router;
